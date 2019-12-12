62426dfcf3edbfdbb605d9dfba0371b14b5db23f
Eduardo Andrade <carlos_duduandrade@hotmail.com>
2019-12-12 17:25:16 -0300

    finish project

--- js/main.js
+++ js/main.js
@@ -1,24 +1,83 @@
 function verifyNumbersGenerate(number) {
-    
-    var boxFeedback;
+    if (number < 10 || number > 100) {
+        document.querySelector('.box-feedback').style.display = 'Block';
 
-    boxFeedback = document.querySelector('.box-feedback');
-
-    
-    if( number < 10 || number > 100 ) {
-        boxFeedback.style.display = 'Block';
         return false;
     }
-
-    boxFeedback.style.display = 'none';
+    document.querySelector('.box-feedback').style.display = 'none';
+    showResult();
 }
 
 function getRandomInt(e) {
     e.preventDefault();
+    reset()
 
-    let qtdNumbers = parseInt(document.getElementById('qtdNumber').value);
-    
+    var qtdNumbers = parseInt(document.getElementById('qtdNumber').value);
+
     verifyNumbersGenerate(qtdNumbers);
+    var numberRandoms = [...Array(qtdNumbers)].map(() => Math.floor(Math.random() * (100 - 10 + 1) + 10));
 
-    let numberRandoms  = [...Array(qtdNumbers)].map(() => Math.floor(Math.random() * (100 - 10 + 1) + 10));
+    generateFields(numberRandoms);
+
+
+}
+
+function generateFields(numbers) {
+    setNumbersRandoms(numbers);
+    orderByCrescent(numbers);
+    orderByDesc(numbers);
+    verifyNumbersOddPair(numbers);
+}
+
+function setNumbersRandoms(data) {
+    setDataResult(data, 'number-random');
+}
+
+function orderByCrescent(data) {
+    var numbersOrdered = data.sort((a, b) => (a > b ? -1 : 1))
+    setDataResult(numbersOrdered, 'numbers-ordered');
+}
+
+function orderByDesc(data) {
+    var numbersOrdered = data.sort()
+    setDataResult(numbersOrdered, 'numbers-desc');
+}
+
+function verifyNumbersOddPair(data) {
+    var odd = [];
+    var pair = [];
+
+    data.map((value, index) => {
+        if (value % 2 == 0) {
+            pair.push(value)
+        } else {
+            odd.push(value)
+        }
+    })
+
+    setDataResult(odd.sort((a, b) => (a > b ? -1 : 1)), 'numbers-odd');
+    setDataResult(pair.sort(), 'numbers-pair');
+}
+
+function setDataResult(data, id) {
+    var box;
+    box = document.getElementById(id);
+
+    data.map((values, index) => {
+        var liElement = document.createElement('li');
+        liElement.innerHTML = data[index];
+        box.appendChild(liElement);
+    })
+}
+
+function showResult() {
+    document.querySelector('.box-result').style.display = 'flex';
+}
+
+function reset() {
+    var arrayIds = ['number-random', 'numbers-ordered', 'numbers-desc', 'numbers-odd', 'numbers-pair']
+
+    arrayIds.map((value) => {
+        document.getElementById(value).innerHTML = '';
+    })
 }
